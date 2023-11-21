96e3cbed2b87099cc2f109075766a1e413d789c5
Jesse Lephart <jesselephart@gmail.com>
2023-11-20 17:11:30 -0800

    Adds gallary card outline theme

--- src/app/components/Card.js
+++ src/app/components/Card.js
@@ -5,14 +5,14 @@ export default function Card({
   alt = '',
 }) {
   return (
-    <div className="flex-column rounded shadow-lg bg-gray-100 transition-all hover:scale-105 cursor-pointer">
+    <div className="flex-column rounded outline outline-4 outline-white shadow-lg bg-white transition-all hover:scale-[1.02] cursor-pointer">
       <img className="max-h-[500px]" src={img} alt={description} />
-      <div className="px-6 py-4 bg-gray-100">
+      <div className="px-6 py-4 ">
         <div className="font-bold text-xl mb-2 text-indigo-700">{title}</div>
         <p className="text-gray-800 text-base w-full">{description}</p>
       </div>
-      <div className="px-6 pb-4 flex bg-gray-100">
-        <h2 className="font-semibold text-blue-900 bg-gray-200 flex pl-2 pr-2">{}</h2>
+      <div className="px-6 pb-4 flex">
+        <h2 className="font-semibold text-blue-900 flex pl-2 pr-2">{}</h2>
       </div>
     </div>
   )
--- src/app/gallery/page.js
+++ src/app/gallery/page.js
@@ -3,12 +3,13 @@ import Navbar from '../components/Navbar'
 import PageTitle from '../components/PageTitle'
 import Card from '../components/Card'
 import Footer from '../components/Footer'
+import PageContentStyled from '../../../csci31final/src/app/components/PageContentStyled'
 
 import { createClient } from '@supabase/supabase-js'
 
 // Create a single supabase client for interacting with your database
 const supabase = createClient('https://ahsxmxdtdhqtdxcxadwp.supabase.co', process.env.SUPABASE_SERVICE_ROLE_KEY)
-export const revalidate = 0
+export const revalidate = 3000
 
 export default async function Gallary() {
   const { data: cards, error } = await supabase.from('cards').select()
