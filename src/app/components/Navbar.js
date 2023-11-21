ba34d2db219068651c35405bf798a52026d31e0b
Jesse Lephart <jesselephart@gmail.com>
2023-11-20 21:27:32 -0800

    Removes heroicon until I can get a better one

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -11,7 +11,7 @@ const menuItems = [
 export default function Navbar() {
   return (
     <div className="flex justify-between gap-4 m-12 items-center">
-      <PaintBrushIcon className="h-12 w-12 text-indigo-700 drop-shadow-md"></PaintBrushIcon>
+      <PaintBrushIcon className="h-12 w-12 text-indigo-700 text-opacity-0"></PaintBrushIcon>
       <div className="flex gap-4 flex-wrap">
         {menuItems.map((item, index) => (
           <a
--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,6 @@ import Image from 'next/image'
 import Navbar from './components/Navbar'
 import PageTitle from './components/PageTitle'
 import PageContent from './components/PageContent'
-import Card from './components/Card'
 import Footer from './components/Footer'
 
 export default function Home() {
