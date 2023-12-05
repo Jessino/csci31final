749af59b6769b29c4770bcf48bb2a669bbf40c9f
Jesse Lephart <jesselephart@gmail.com>
2023-12-04 20:02:27 -0800

    add commission component and finish products page

---
+++ src/app/components/Commission.js
@@ -0,0 +1,37 @@
+export default function Commission({
+  title = 'Title',
+  img = 'https://veriaart.carrd.co/assets/images/gallery02/3d89b20a_original.jpg?v=cf152a5c',
+  sketchPrice = '$100',
+  basicPrice = '$100',
+  fullPrice = '$100',
+  additional = 'Nil',
+}) {
+  return (
+    <div className="flex w-full flex-row mb-6 mt-6">
+      <div className="flex flex-col flex-1 items-center">
+        <div className="text-xl mb-2 mt-2">{title}</div>
+        <div className="flex w-full flex-row justify-around mb-2 mt-2">
+          <div>
+            <p>Sketch</p>
+            <p>{sketchPrice}</p>
+          </div>
+          <div>
+            <p>Basic</p>
+            <p>{basicPrice}</p>
+          </div>
+          <div>
+            <p>Full</p>
+            <p>{fullPrice}</p>
+          </div>
+        </div>
+        <div className="mt-2 mb-2">
+          <p className="text-center">Aditional fees:</p>
+          <p className="text-center">{additional}</p>
+        </div>
+      </div>
+      <div className="flex-1">
+        <img className="" src={img} alt={title} />
+      </div>
+    </div>
+  )
+}
--- src/app/products/page.js
+++ src/app/products/page.js
@@ -1,7 +1,9 @@
 import Image from 'next/image'
 import Navbar from '../components/Navbar'
 import PageTitle from '../components/PageTitle'
-import PageContent from '../components/PageContent'
+import PageContentStyled from '../components/PageContentStyled'
+import Footer from '../components/Footer'
+import Commission from '../components/Commission'
 
 export default function Products() {
   return (
@@ -9,9 +11,48 @@ export default function Products() {
       <Navbar />
       <div className="m-12">
         <PageTitle title="Products" />
-        <PageContent content="Our products are the best!" />
-        <PageContent content="The really are!" />
+        <PageContentStyled>
+          <div className=" flex flex-col w-full items-center">
+            <div className="max-w-xl flex flex-col  items-center content-center">
+              <h1 className="text-2xl text-indigo-700 font-bold mb-3 italic">Commission info</h1>
+              <h1 className="text-2xl text-indigo-700 font-bold mb-3 italic">Note:</h1>
+              <p>
+                By commissioning me, Veria Art, you have agreed to the{' '}
+                <a className="underline" href="/tos">
+                  terms of service
+                </a>
+                . I only accept Venmo and PayPal (USD) too. If you have any questions or concerns, please email me or
+                message me on instagram
+              </p>
+              <Commission
+                title="Bust"
+                img="https://veriaart.carrd.co/assets/images/gallery02/3d89b20a_original.jpg?v=cf152a5c"
+                sketchPrice="$10"
+                basicPrice="$20"
+                fullPrice="$35"
+                additional="Background: $5 (simple) $10 (medium) Characters: $10 Animals: $10"
+              />
+              <Commission
+                title="Half Body"
+                img="https://veriaart.carrd.co/assets/images/gallery03/a79bce96_original.jpg?v=cf152a5c"
+                sketchPrice="$25"
+                basicPrice="$45"
+                fullPrice="$65"
+                additional="Background: $10 (simple) $20 (medium) Characters: $20 Animals: $15"
+              />
+              <Commission
+                title="Full Body"
+                img="https://veriaart.carrd.co/assets/images/gallery01/15148fcf_original.jpg?v=cf152a5c"
+                sketchPrice="$40"
+                basicPrice="$60"
+                fullPrice="$85"
+                additional="Background: $20 (simple) $30 (medium) Characters: $40 Animals: $20"
+              />
+            </div>
+          </div>
+        </PageContentStyled>
       </div>
+      <Footer />
     </div>
   )
 }
